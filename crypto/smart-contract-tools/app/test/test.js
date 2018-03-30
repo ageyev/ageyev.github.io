968a2891b869ec58d470e09ff1a8c453307d546d
Viktor <ageyev@gmail.com>
2018-03-30 10:40:55 +0300

    add text<>hex converter

--- crypto/smart-contract-tools/app/test/test.js
+++ crypto/smart-contract-tools/app/test/test.js
@@ -53,6 +53,30 @@
 
             $log.info("controller ", controller_name, "started");
 
+            $scope.bytes32toTextInput = null;
+            $scope.bytes32toTextResult = null;
+
+            $scope.textToHexInput = null;
+            $scope.textToHexResult = null;
+
+            $scope.toHex = function () {
+                $log.debug("$scope.toHex input: ", $scope.textToHexInput);
+                // https://github.com/ethereum/wiki/wiki/JavaScript-API#web3tohex
+                // $scope.textToHexResult = $rootScope.web3.toHex($scope.textToHexInput);
+                $scope.textToHexResult = $rootScope.web3.fromUtf8($scope.textToHexInput, 32);
+                // $scope.$apply(); //
+                $log.debug("$scope.toHex result: ", $scope.textToHexResult);
+            };
+
+            $scope.toText = function () {
+                $log.debug("$scope.toText input: ", $scope.bytes32toTextInput);
+                // https://github.com/ethereum/wiki/wiki/JavaScript-API#web3tohex
+                $scope.bytes32toTextResult = $rootScope.web3.toUtf8($scope.bytes32toTextInput, 32);
+                // $scope.$apply();
+
+                $log.debug("$scope.toText result: ", $scope.textToHexResult);
+            }
+
         } // end function homeCtl
     ]);
 })();

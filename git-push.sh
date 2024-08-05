df794429508e8db61347aa62a2d075d25c51e78b
ageyev <ageyev@gmail.com>
2024-08-05 23:23:03 +0300

    update push script

--- git-push.sh
+++ git-push.sh
@@ -2,8 +2,16 @@
 source ./.env;
 
 # git init 
+
+# add remote repository 
+# git remote remove origin # remove remote 'origin' repository 
+# git remote add gitlab "git@gitlab.com:${GIT_USER_NAME}/${APP_NAME}.git"
+# git remote add github "git@github.com:${GIT_USER_NAME}/${APP_NAME}.git" 
+
 git config --local user.name "$GIT_USER_NAME";
 git config --local user.email "$GIT_USER_EMAIL";
+
+# git config --local -l 
 
 ssh-add -D 
 ssh-add "${GITHUB_KEY_PATH}";

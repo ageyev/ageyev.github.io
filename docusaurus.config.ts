36a7fa9afe1529788fb055d9359b0c902274f252
ageyev <ageyev@gmail.com>
2024-08-07 10:31:06 +0300

    initial setup for docusaurus based blog and docs

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -1,22 +1,23 @@
 import {themes as prismThemes} from 'prism-react-renderer';
 import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
+//
 
 const config: Config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: 'International Law', // 'My Site', // "title" is not allowed to be empty
+  tagline: 'by Viktor Ageyev',
+  favicon: 'img/favicon/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://international-law.info',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'ageyev', // Usually your GitHub org/username.
+  projectName: 'ageyev.github.io', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
@@ -26,7 +27,7 @@ const config: Config = {
   // may want to replace "en" with "zh-Hans".
   i18n: {
     defaultLocale: 'en',
-    locales: ['en'],
+    locales: ['en', 'ru', 'uk'], //
   },
 
   presets: [
@@ -34,18 +35,24 @@ const config: Config = {
       'classic',
       {
         docs: {
+          // see: https://docusaurus.io/docs/docs-introduction#docs-only-mode
+          // routeBasePath: '/', // Serve the docs at the site's root
+
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/ageyev/ageyev.github.io/',
+          showLastUpdateAuthor: true, // git username from the last commit
+          showLastUpdateTime: true, // git last commit time
         },
         blog: {
-          showReadingTime: true,
+          // see: https://docusaurus.io/docs/blog#blog-only-mode
+          routeBasePath: '/', // Serve the blog at the site's root
+
+          showReadingTime: false, // default: true
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          // editUrl: 'https://github.com/ageyev/ageyev.github.io/',
         },
         theme: {
           customCss: './src/css/custom.css',
@@ -56,72 +63,60 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: 'img/docusaurus-social-card.jpg',
+    image: 'img/docusaurus-social-card.jpg', // TODO: change
+
     navbar: {
-      title: 'My Site',
+      // --- Navbar Title
+      title: '', // TODO: add site title to Navbar
       logo: {
-        alt: 'My Site Logo',
+        alt: 'Logo',
         src: 'img/logo.svg',
       },
+
+      // --- Navbar Links
       items: [
-        {
+        { // --- Link to docs
           type: 'docSidebar',
           sidebarId: 'tutorialSidebar',
           position: 'left',
-          label: 'Tutorial',
+          label: 'Texts', // 'Tutorial',
         },
-        {to: '/blog', label: 'Blog', position: 'left'},
-        {
-          href: 'https://github.com/facebook/docusaurus',
+        { // --- Link to blog
+          to: '/', // '/blog' > '/' , see https://docusaurus.io/docs/blog#blog-only-mode
+          label: 'Blog',
+          position: 'left'
+        },
+        { // --- Link to GitHub
+          href: 'https://github.com/ageyev/ageyev.github.io/',
           label: 'GitHub',
           position: 'right',
         },
       ],
     },
+
+    // --- Footer
+    // see:
+    // https://github.com/facebook/docusaurus/blob/7be1feaa0a6fbfd18faec8fe0488d24a61b15596/website/docusaurus.config.ts#L704
     footer: {
       style: 'dark',
       links: [
-        {
-          title: 'Docs',
-          items: [
-            {
-              label: 'Tutorial',
-              to: '/docs/intro',
-            },
-          ],
+        { // X.com
+          html: `
+                <a href="https://x.com/ageyev" target="_blank" title="X.com" class="footer_icon">
+                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="footer_icon"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+                </a>
+              `,
         },
-        {
-          title: 'Community',
-          items: [
-            {
-              label: 'Stack Overflow',
-              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-            },
-            {
-              label: 'Discord',
-              href: 'https://discordapp.com/invite/docusaurus',
-            },
-            {
-              label: 'Twitter',
-              href: 'https://twitter.com/docusaurus',
-            },
-          ],
-        },
-        {
-          title: 'More',
-          items: [
-            {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
-            },
-          ],
+        { // Facebook
+          html: `
+                <a href="https://www.facebook.com/viktor.ageyev" target="_blank" title="Facebook" class="footer_icon">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="footer_icon"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/></svg>
+                </a>
+              `,
         },
       ],
-      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      // copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      copyright: `Copyright © 2024, Viktor Ageyev`,
     },
     prism: {
       theme: prismThemes.github,
--- project_setup.sh
+++ project_setup.sh
@@ -2,19 +2,29 @@
 # npx create-docusaurus@latest [name] [template] [rootDir]
 name="docusaurus";
 tempDir="${HOME}/TEMP/";
-npx create-docusaurus@latest $name classic $tempDir --typescript 
-mv ${tempDir}${name}/* . # all except .gitignore
+
+npx create-docusaurus@latest $name classic "${tempDir}" --typescript
+mv "${tempDir}${name}"/* . # all except .gitignore
 rm -r "${tempDir}${name}"
+
+# types for react-dom
+# for:
+# import {renderToStaticMarkup} from "react-dom/server";
+# https://react.dev/reference/react-dom/server/renderToStaticMarkup
+npm install -S @types/react-dom
+
 
 # YouTube embed videos
 # see: https://github.com/facebook/docusaurus/blob/main/website/src/pages/index.tsx
 # https://www.npmjs.com/package/react-lite-youtube-embed
 npm install -S react-lite-youtube-embed
 
-# Font Awesome for React
+# Font Awesome
+# see: https://docusaurus.community/knowledge/design/icons/fontawesome/
+#
 # https://fontawesome.com/docs/web/use-with/react/
 # https://fontawesome.com/docs/web/use-with/react/style
-# see:
+#
 # https://fontawesome.com/search?o=r&m=free&s=solid (1390 icons)
 # https://fontawesome.com/search?o=r&m=free&s=regular (163 icons)
 # https://fontawesome.com/search?o=r&f=brands (484 icons)

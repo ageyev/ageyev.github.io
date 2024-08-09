f0e3db574f23f58f5b7e3486550935951ff6a2bf
ageyev <ageyev@gmail.com>
2024-08-09 10:44:54 +0300

    change social icons

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -4,7 +4,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 //
 
 const config: Config = {
-  title: 'International Law', // 'My Site', // "title" is not allowed to be empty
+  title: 'International Law etc.', // 'My Site', // "title" is not allowed to be empty
   tagline: 'by Viktor Ageyev',
   favicon: 'img/favicon/favicon.ico',
 
@@ -101,17 +101,32 @@ const config: Config = {
     footer: {
       style: 'dark',
       links: [
+        {
+          label: '[Texts]',
+          to: '/docs/intro',
+        },
+        {
+          label: '[Blog]',
+          to: '/',
+        },
+        { // Youtube
+          html: `
+                <a href="https://www.youtube.com/@international-law-info" target="_blank" title="YouTube" class="footer_icon">
+                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" class="youtube icon"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M549.7 124.1c-6.3-23.7-24.8-42.3-48.3-48.6C458.8 64 288 64 288 64S117.2 64 74.6 75.5c-23.5 6.3-42 24.9-48.3 48.6-11.4 42.9-11.4 132.3-11.4 132.3s0 89.4 11.4 132.3c6.3 23.7 24.8 41.5 48.3 47.8C117.2 448 288 448 288 448s170.8 0 213.4-11.5c23.5-6.3 42-24.2 48.3-47.8 11.4-42.9 11.4-132.3 11.4-132.3s0-89.4-11.4-132.3zm-317.5 213.5V175.2l142.7 81.2-142.7 81.2z"/></svg>
+                </a>
+              `,
+        },
         { // X.com
           html: `
                 <a href="https://x.com/ageyev" target="_blank" title="X.com" class="footer_icon">
-                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="footer_icon"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
+                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="twitter icon"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M389.2 48h70.6L305.6 224.2 487 464H345L233.7 318.6 106.5 464H35.8L200.7 275.5 26.8 48H172.4L272.9 180.9 389.2 48zM364.4 421.8h39.1L151.1 88h-42L364.4 421.8z"/></svg>
                 </a>
               `,
         },
         { // Facebook
           html: `
                 <a href="https://www.facebook.com/viktor.ageyev" target="_blank" title="Facebook" class="footer_icon">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="footer_icon"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/></svg>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" class="facebook icon"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path d="M512 256C512 114.6 397.4 0 256 0S0 114.6 0 256C0 376 82.7 476.8 194.2 504.5V334.2H141.4V256h52.8V222.3c0-87.1 39.4-127.5 125-127.5c16.2 0 44.2 3.2 55.7 6.4V172c-6-.6-16.5-1-29.6-1c-42 0-58.2 15.9-58.2 57.2V256h83.6l-14.4 78.2H287V510.1C413.8 494.8 512 386.9 512 256h0z"/></svg>
                 </a>
               `,
         },

c47393bf0e2bc37f7598bf3d73bab43b829b81db
ageyev <ageyev@gmail.com>
2024-11-15 16:05:27 +0200

    run build script

---
+++ build/ru/assets/js/aba21aa0.f7c5ade7.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkageyev_github_io=self.webpackChunkageyev_github_io||[]).push([[742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);

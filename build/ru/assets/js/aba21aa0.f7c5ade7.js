c01ef15a71332314cb265be2a36574cf17de509d
ageyev <ageyev@gmail.com>
2024-11-16 09:26:48 +0200

    update

--- build/ru/assets/js/aba21aa0.f7c5ade7.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkageyev_github_io=self.webpackChunkageyev_github_io||[]).push([[742],{7093:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
--- build/uk/assets/js/36994c47.16e75d11.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkageyev_github_io=self.webpackChunkageyev_github_io||[]).push([[858],{5516:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-blog","id":"default"}')}}]);

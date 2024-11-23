ff07b6ca451a5535737acf831162e3af4e091163
ageyev <ageyev@gmail.com>
2024-11-23 20:21:27 +0200

    run build script

--- build/uk/assets/js/b9c0851c.f3594f36.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkageyev_github_io=self.webpackChunkageyev_github_io||[]).push([[632],{5727:e=>{e.exports=JSON.parse('{"tags":[{"label":"Docusaurus","permalink":"/uk/tags/docusaurus","description":"Docs related to the Docusaurus framework","count":1}]}')}}]);

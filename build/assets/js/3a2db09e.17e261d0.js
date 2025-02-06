bad6a1d325b2213f0e884cd58df0e0c848c9fa6a
ageyev <ageyev@gmail.com>
2025-02-06 16:17:49 +0200

    run build script

---
+++ build/assets/js/3a2db09e.17e261d0.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkageyev_github_io=self.webpackChunkageyev_github_io||[]).push([[8121],{8070:e=>{e.exports=JSON.parse('{"tags":[{"label":"Viktor Ageyev","permalink":"/blog/tags/ageyev","description":"Viktor Ageyev","count":2},{"label":"Israel","permalink":"/blog/tags/israel","description":"Israel","count":2}]}')}}]);
--- build/assets/js/814f3328.c25fa673.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkageyev_github_io=self.webpackChunkageyev_github_io||[]).push([[472],{5513:e=>{e.exports=JSON.parse('{"title":"Recent posts","items":[{"title":"October 7","permalink":"/blog/october7","unlisted":false,"date":"2024-10-07T00:00:00.000Z"}]}')}}]);

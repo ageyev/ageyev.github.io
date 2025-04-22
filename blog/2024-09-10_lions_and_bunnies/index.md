5e168cdc162c27ada24e5c6e7ef7885046ef6e9c
ageyev <ageyev@gmail.com>
2025-04-22 18:49:03 +0300

    start a blog post about lions and bunnies

---
+++ blog/2024-09-10_lions_and_bunnies/index.md
@@ -0,0 +1,13 @@
+---
+slug: lions_and_bunnies
+title: Lions and Bunnies
+authors: [ageyev]
+tags: [holy-land]
+draft: false
+date: 2024-09-10
+description: "Analysis of the conflict in israeli society regarding war strategy"
+lang: en
+---
+
+![DALL·E 2024-09-10 16.06.05 - A lion and a bunny sitting at a table, each holding books and engaged in a lively scientific debate](/img/Illustrations/blog/2024-09-10_lions_and_bunnies/2024-09-10_a_lion_and_a_bunny_sitting_at_a_table.webp) 
+
--- new_blog_post_i18n.sh
+++ new_blog_post_i18n.sh
@@ -1,9 +1,11 @@
 # new blog post
 
-BLOG="blog"
-RU_BLOG="i18n/ru/docusaurus-plugin-content-blog/"
-UK_BLOG="i18n/uk/docusaurus-plugin-content-blog/"
+#BLOG="blog"
+RU_BLOG="./i18n/ru/docusaurus-plugin-content-blog/"
+UK_BLOG="./i18n/uk/docusaurus-plugin-content-blog/"
 
-NEW_BLOG_POST=$BLOG/2025-04-18-What_Gaza_was_counting_on
+NEW_BLOG_POST="./blog/2024-09-10_lions_and_bunnies"
 cp -r $NEW_BLOG_POST $RU_BLOG
+echo "coping" $NEW_BLOG_POST "to" $RU_BLOG
 cp -r $NEW_BLOG_POST $UK_BLOG
+echo "coping" $NEW_BLOG_POST "to" $UK_BLOG

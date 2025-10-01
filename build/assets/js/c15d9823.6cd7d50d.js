910c2b93fb55a6729ce565d66386ec0633b93a0a
ageyev <ageyev@gmail.com>
2025-10-01 10:45:43 +0300

    ensure a trailing slash at the end of URLs/links

--- build/assets/js/c15d9823.6cd7d50d.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkageyev_github_io=self.webpackChunkageyev_github_io||[]).push([[8146],{9328:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/blog","page":1,"postsPerPage":5,"totalPages":3,"totalCount":12,"nextPage":"/blog/page/2","blogDescription":"Ubi societas, ibi ius","blogTitle":"International Law etc."}}')}}]);
--- docs/intro.md
+++ docs/intro.md
@@ -11,4 +11,3 @@ slug: /
 This site is primarily about international law and topics closely or not so closely related to it, like local law, and also philosophy, religion and history. 
 
 To navigate the site, use the sidebar and header menu.
-

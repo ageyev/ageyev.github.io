0b60d49e3acea56638129ac3298d69dfeb2c8552
ageyev <ageyev@gmail.com>
2025-10-09 17:14:59 +0300

    add blog post "A deal with the Devil"

---
+++ build/assets/js/7aafb598.fe3a17ba.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkageyev_github_io=self.webpackChunkageyev_github_io||[]).push([[3382],{4614:e=>{e.exports=JSON.parse('{"metadata":{"permalink":"/blog/page/3","page":3,"postsPerPage":5,"totalPages":3,"totalCount":13,"previousPage":"/blog/page/2","blogDescription":"Ubi societas, ibi ius","blogTitle":"International Law etc."}}')}}]);

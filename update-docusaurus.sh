38d06cbc70fd037db0f8c0e651680154600039a4
ageyev <ageyev@gmail.com>
2025-10-24 00:17:42 +0300

    update Docusaurus 3.9.1 → 3.9.2

--- update-docusaurus.sh
+++ update-docusaurus.sh
@@ -3,8 +3,8 @@ docusaurus clear
 
 npm i @docusaurus/core@latest @docusaurus/plugin-content-blog@latest @docusaurus/plugin-ideal-image@latest @docusaurus/preset-classic@latest @docusaurus/module-type-aliases@latest @docusaurus/tsconfig@latest @docusaurus/types@latest
 
-npm audit fix
+#npm audit fix
 
-npx update-browserslist-db@latest
+#npx update-browserslist-db@latest
 
 

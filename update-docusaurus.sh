20a57a122b6f40738ae864abdfd92b634720a619
ageyev <ageyev@gmail.com>
2025-10-25 17:55:53 +0300

    update

--- update-docusaurus.sh
+++ update-docusaurus.sh
@@ -3,8 +3,5 @@ docusaurus clear
 
 npm i @docusaurus/core@latest @docusaurus/plugin-content-blog@latest @docusaurus/plugin-ideal-image@latest @docusaurus/preset-classic@latest @docusaurus/module-type-aliases@latest @docusaurus/tsconfig@latest @docusaurus/types@latest
 
-#npm audit fix
-
-#npx update-browserslist-db@latest
 
 

c426e57e56c22cc7a94f962bf0c8d2f4d28b3b94
ageyev <ageyev@gmail.com>
2026-05-01 19:10:01 +0300

    start doc about tahrif

---
+++ i18n/uk/docusaurus-plugin-content-docs/current/theology/tahrif.md
@@ -0,0 +1,11 @@
+---
+title: "Tahrif"
+id: tahrif
+hide_title: true
+draft: true
+tags: [theology, Islam]
+lang: en
+description: "Returning to Muslims the sacred scriptures that are the foundation of Islam"
+---
+
+
--- new_doc_i18n.sh
+++ new_doc_i18n.sh
@@ -9,5 +9,6 @@ UK_DOCS='i18n/uk/docusaurus-plugin-content-docs/current/';
 #NEW_DOC='Holy-Land/belligerent_occupation.md'
 #NEW_DOC='Holy-Land/the_term_palestine.md'
 #NEW_DOC='theology/theology.md'
-NEW_DOC='theology/al-aqsa.md'
+#NEW_DOC='theology/al-aqsa.md'
+NEW_DOC='theology/tahrif.md'
 cp -r $DOCS$NEW_DOC $RU_DOCS$NEW_DOC && cp -r $DOCS$NEW_DOC $UK_DOCS$NEW_DOC
